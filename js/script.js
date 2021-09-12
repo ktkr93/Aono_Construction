71af520abdd25dd40f3f843a9b17a6c6c1c445fd
ktkr93 <ktkr93@gmail.com>
2021-09-12 19:46:33 +0900

    modify css,js修正

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 $(function() {
     $('.btn-trigger').on('click', function() {
         $(this).toggleClass('active');
+        $('.header-nav').toggleClass('header-nav1');
+        $('.header-nav1').fadeToggle(500);
         return false;
     });
 });
