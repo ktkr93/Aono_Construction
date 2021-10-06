c2efd5111752fa9d46315f9dcd140f1f1fafdabb
ktkr93 <ktkr93@gmail.com>
2021-10-06 18:30:40 +0900

    modify ハンバーガーメニューを修正

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
 $(function() {
     $('.btn-trigger').on('click', function() {
         $(this).toggleClass('active');
-        $('.header-nav').toggleClass('header-nav1');
-        $('.header-nav1').fadeToggle(500);
-        return false;
+        $('.nav-wrapper').fadeToggle(500);
+        $('body').toggleClass('noscroll');
     });
 });
