ff18b5278b52c33eecb2750ff54b7185d9b4c96c
ktkr93 <ktkr93@gmail.com>
2022-03-03 11:08:08 +0900

    すべてのファイルを更新

--- js/script.js
+++ js/script.js
@@ -1,8 +1,49 @@
 $(function() {
     $('.btn-trigger').on('click', function() {
         $(this).toggleClass('active');
-        $('.header-nav').toggleClass('header-nav1');
-        $('.header-nav1').fadeToggle(500);
-        return false;
+        $('.nav-wrapper').fadeToggle(500);
+        $('body').toggleClass('noscroll');
     });
 });
+
+//ローディングの設定
+//テキストのカウントアップの設定
+var bar = new ProgressBar.Line(splash_text, { //id名を指定
+    strokeWidth: 0, //進捗ゲージの太さ
+    duration: 1000, //時間指定(1000＝1秒)
+    trailWidth: 0, //線の太さ
+    text: { //テキストの形状を直接指定	
+        style: { //天地中央に配置
+            position: 'absolute',
+            left: '50%',
+            top: '50%',
+            padding: '0',
+            margin: '0',
+            transform: 'translate(-50%,-50%)',
+            'font-size': '1.2rem',
+            color: '#000',
+        },
+        autoStyleContainer: false //自動付与のスタイルを切る
+    },
+    step: function(state, bar) {
+        bar.setText(Math.round(bar.value() * 100) + ' %'); //テキストの数値
+    }
+});
+
+//アニメーションスタート
+bar.animate(1.0, function() { //バーを描画する割合を指定します 1.0 なら100%まで描画します
+    $("#splash").delay(500).fadeOut(800); //アニメーションが終わったら#splashエリアをフェードアウト
+});
+//ローディングの設定終了
+
+//スライダーの設定
+$('.slider').slick({
+    autoplay: false,
+    dots: true,
+    arrows: true,
+    slidesToShow: 1,
+    slideToScroll: 1,
+
+});
+
+//スライダーの設定終了
